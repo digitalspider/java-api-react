eb817d78a2542f4d84ddf80be015d54fff35173f
David Vittor <david.vittor@mbww.com>
2019-05-13 18:38:01 +1000

    Login page

--- ui/src/containers/LoginPage.js
+++ ui/src/containers/LoginPage.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Section from '../components/common/Section';
+import { Input, Button, Grid } from '@material-ui/core';
+import Api from '../utils/Api';
 
 const styles = (theme) => ({
   section: {
@@ -10,6 +12,16 @@ const styles = (theme) => ({
 });
 
 class LoginPage extends Component {
+  handleLogin() {
+    let username = 'admin';
+    let password = 'admin';
+    let data = {
+      username: username,
+      password: password,
+    }
+    Api.post('/user/login', data);
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -17,6 +29,29 @@ class LoginPage extends Component {
         <Typography variant="h4" gutterBottom>
         Login Page
         </Typography>
+        <Grid>
+          <Grid item>
+            <Input inputType='text' placeholder='Username' value=''/>
+          </Grid>
+          <Grid item>
+            <Input inputType='password' placeholder='Password' value=''/>
+          </Grid>
+        </Grid>
+        <Button
+            variant='raised'
+            color="primary"
+            onClick={this.handleLogin}
+            style={{marginRight: '10px'}}
+          >
+            Login
+          </Button>
+          <Button
+            variant='raised'
+            onClick={this.handleForgot}
+            color="secondary"
+          >
+            Forgot Password?
+          </Button>
       </Section>
     );
   }
